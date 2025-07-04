6f62f028bd34675fd9bf5407e649ae8dfcfe6096
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-04 20:39:20 +0530

    Create Generics.kt

---
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/oop/Generics.kt
@@ -0,0 +1,28 @@
+package com.trying.kotlin.oop
+
+private class LivingBeing<AnyLivingBeing>(private val livingBeing: AnyLivingBeing) {
+    fun getLivingBeing(): AnyLivingBeing = livingBeing
+}
+
+private class HumanThing {
+    fun introduce() {
+        println(message = "I'm a human being.")
+    }
+}
+
+private class Animal {
+    fun introduce() {
+        println(message = "I'm a animal.")
+    }
+}
+
+fun main() {
+    val livingBeing1 = LivingBeing(livingBeing = Animal())
+    val livingBeing2 = LivingBeing(livingBeing = HumanThing())
+
+    val animal1 = livingBeing1.getLivingBeing()
+    animal1.introduce()
+
+    val human = livingBeing2.getLivingBeing()
+    human.introduce()
+}

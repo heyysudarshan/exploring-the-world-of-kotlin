430bf19e1699495e8674267bb3a3588c08c35382
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-04 19:29:10 +0530

    Update BuildString.kt

--- trying-kotlin/src/main/kotlin/com/trying/kotlin/strings/BuildString.kt
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/strings/BuildString.kt
@@ -6,7 +6,6 @@ fun main() {
     val message = buildString {
         repeat(times = 20) {
             val randomId = UUID.randomUUID().toString()
-
             append("Hello from random person with id: $randomId")
             appendLine(value = "!")
         }

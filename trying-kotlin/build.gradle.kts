de4fc0111de39561ac14207a62a91f38ce6591a8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-02 17:51:55 +0530

    Set JVM toolchain and compiler options for trying-kotlin module in build configuration

--- trying-kotlin/build.gradle.kts
+++ trying-kotlin/build.gradle.kts
@@ -1,9 +1,21 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
 group = libs.versions.tryingKotlin.group.get()
 version = libs.versions.tryingKotlin.version.get()
+
+kotlin {
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of("21")
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+    }
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
 
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/TryingKotlin"))

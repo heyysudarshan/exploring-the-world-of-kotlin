34cd5e023904ab937f9b70fa5d75f961e5841cbf
= <=>
2025-08-09 11:42:04 +0530

    Set group and version for trying-ktor module

--- trying-ktor/build.gradle.kts
+++ trying-ktor/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.compiler)
 }
 
+group = libs.versions.tryingKtor.group.get()
+version = libs.versions.tryingKtor.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/TryingKtor"))

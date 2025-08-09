1353a30da590f49c8eb8422243b42bb4200b2735
= <=>
2025-08-09 13:43:59 +0530

    Configure Java 23 toolchain and set JVM target in trying-ktor module

--- trying-ktor/build.gradle.kts
+++ trying-ktor/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
     alias(libs.plugins.ktor)
@@ -5,6 +7,22 @@ plugins {
 
 group = libs.versions.tryingKtor.group.get()
 version = libs.versions.tryingKtor.version.get()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("23")
+    }
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_23
+}
+
+dependencies {
+
+}
 
 // Custom build directory
 layout.buildDirectory.set(file(path = "$rootDir/.build/TryingKtor"))

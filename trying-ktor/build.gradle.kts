fbcf2fa5fd2501d0b53b1963c4d859c341dbc159
= <=>
2025-08-09 18:45:07 +0530

    Apply "org.jetbrains.kotlin.plugin.serialization" plugin to the trying-ktor module

--- trying-ktor/build.gradle.kts
+++ trying-ktor/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.compiler)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = libs.versions.tryingKtor.group.get()

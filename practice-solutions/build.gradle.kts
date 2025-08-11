7cd6e466f4412e2856ca0efb1edaab8a3eaf093b
= <=>
2025-08-11 17:01:25 +0530

    Configure Kotlin compiler and upgrade JVM/Java versions to 23 in the practice-solutions module

--- practice-solutions/build.gradle.kts
+++ practice-solutions/build.gradle.kts
@@ -1,5 +1,22 @@
-plugins {
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin.compiler)
+}
+
+group = libs.versions.practiceSolutions.group.get()
+version = libs.versions.practiceSolutions.version.get()
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of("23")
+    }
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_23
 }
 
 // Custom build directory

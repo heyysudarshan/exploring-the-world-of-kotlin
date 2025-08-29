cb3803ae085a28d057b59aec0a575a739b50c4a7
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-08-29 15:04:41 +0530

    Create PropertyAccessors.kt with an example of custom getter and setter in Kotlin

---
+++ trying-kotlin/src/main/kotlin/com/trying/kotlin/oop/PropertyAccessors.kt
@@ -0,0 +1,28 @@
+package com.trying.kotlin.oop
+
+@Suppress("RedundantGetter")
+private class BroadcastManager {
+    var currentChannel = 0
+        get() = field
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+}
+
+fun main() {
+    val broadcastManager = BroadcastManager()
+
+    repeat(times = 102) {
+        println(message = "Increasing the current channel by 1.")
+        broadcastManager.currentChannel++
+        println(message = "The current channel is: ${broadcastManager.currentChannel}.\n")
+    }
+
+    repeat(times = 102) {
+        println(message = "Decreasing the current channel by 1.")
+        broadcastManager.currentChannel--
+        println(message = "The current channel is: ${broadcastManager.currentChannel}.\n")
+    }
+}

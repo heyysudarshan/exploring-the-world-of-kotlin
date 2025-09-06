01838b3e247bfd5ca68f87cbaaac2654b7baae67
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-09-06 16:20:56 +0530

    Add a route to serve HTML document using GET method

---
+++ trying-ktor/src/main/kotlin/com/trying/ktor/routing/HtmlRoute.kt
@@ -0,0 +1,28 @@
+package com.trying.ktor.routing
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+private val htmlContent = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>A Sample Document Served by Ktor</title>
+    </head>
+    <body>
+        <h1>Hi! This webpage is powered by a Ktor server.</h1>
+    </body>
+    </html>
+""".trimIndent()
+
+fun Application.htmlRoute() {
+    routing {
+        get("/html-text") {
+            call.respondText(contentType = ContentType.parse("text/html"), text = htmlContent)
+        }
+    }
+}
